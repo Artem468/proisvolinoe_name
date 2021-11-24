609472d9faaad9c29a364b35abcb733f27ab113d
Artem468 <artem.belykh.ru@gmail.com>
2021-11-24 22:15:22 +0200

    vrode tak

--- main.py
+++ main.py
@@ -2,18 +2,17 @@ import sys
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QMainWindow, QApplication
-from PyQt5 import uic
+from neui import Ui_MainWindow
 from PyQt5.QtGui import QPainter, QColor
 from random import randint
 
 
-class Example(QMainWindow):
+class Example(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("UI.ui", self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.drawf)
         self.initUI()
-
 
     def drawf(self):
         self.flag = True
@@ -39,9 +38,8 @@ class Example(QMainWindow):
     def draw(self):
         r = randint(1, 100)
         self.coords_ = [randint(100, 700), randint(100, 500)]
-        self.qp.setBrush(QColor(255, 255, 0))
+        self.qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
         self.qp.drawEllipse(self.coords_[0], self.coords_[1], r, r)
-
 
 
 if __name__ == '__main__':
