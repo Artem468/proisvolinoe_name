a3ee3e651a9bc583e1d8e8973e98d185ca95d122
Artem468 <artem.belykh.ru@gmail.com>
2021-11-29 22:00:00 +0200

    Espresso

--- main.py
+++ main.py
@@ -1,48 +1,35 @@
+import sqlite3
 import sys
-
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QMainWindow, QApplication
-from neui import Ui_MainWindow
-from PyQt5.QtGui import QPainter, QColor
-from random import randint
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
 
 
-class Example(QMainWindow, Ui_MainWindow):
+class Example(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
-        self.pushButton.clicked.connect(self.drawf)
-        self.initUI()
+        uic.loadUi("main.ui", self)
+        self.pushButton.clicked.connect(self.open)
 
-    def drawf(self):
-        self.flag = True
-        self.update()
+    def open(self):
+        self.con = sqlite3.connect("coffee.sqlite3")
+        cur = self.con.cursor()
+        result = cur.execute('''SELECT * FROM Coffee''').fetchall()
+        self.tableWidget.setRowCount(len(result))
+        self.tableWidget.setColumnCount(len(result[0]))
+        self.titles = [description[0] for description in cur.description]
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+        self.modified = {}
 
-    def paintEvent(self, event):
-        if self.flag:
-            self.qp = QPainter()
-            self.qp.begin(self)
-            self.draw()
-            self.qp.end()
 
-    def initUI(self):
-        self.setWindowTitle('Рисование')
-        self.coords_ = [0, 0]
-        self.setMouseTracking(True)
-        self.qp = QPainter()
-        self.flag = False
-        self.status = None
-
-        self.show()
-
-    def draw(self):
-        r = randint(1, 100)
-        self.coords_ = [randint(100, 700), randint(100, 500)]
-        self.qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
-        self.qp.drawEllipse(self.coords_[0], self.coords_[1], r, r)
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Example()
+    form = Example()
+    form.show()
+    sys.excepthook = except_hook
     sys.exit(app.exec())
